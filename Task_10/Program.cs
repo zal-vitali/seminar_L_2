c438f22c2067295b6fe4accac73fea7b525909bf
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-02-01 16:00:31 +0300

    Tasks

---
+++ Task_10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите трёхзначное число:");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int result;
+
+if (number < 100 || number > 999)
+{
+   Console.WriteLine("Введённое число не является трёхзначным"); 
+}
+else
+{
+    result = ((number / 10) % 10);
+    Console.WriteLine($"Вторая цифра числа {number}: {result}");
+}
---
+++ Task_13/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.Clear();
+
+Console.WriteLine("Будем искать третью цифру числа. Введите произвольное число");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int result = 0;
+int tmpnumber;
+
+if (number < 100)
+{
+    Console.WriteLine($"В числе {number} третьей цифры нет.");
+}
+else
+{
+    if (number < 1000)
+    {   
+        result = (number % 10);    
+    }
+
+    else
+    {
+        tmpnumber = number;
+        while(tmpnumber >= 1000)
+        {
+            tmpnumber = tmpnumber / 10;  
+            result = tmpnumber % 10;              
+        }
+        
+    }
+
+    Console.WriteLine($"Третья цифра числа {number}: {result}");
+}
